760dfc004b29678d5742d6e9b296637af7da1461
Abhimanyu kumar <166692284+abhi-webdev@users.noreply.github.com>
2025-04-20 18:42:21 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -23,7 +23,7 @@ app.use(express.urlencoded({extended: true}))
 // const allowedOrigins = ["https://authentication-frontend-2ygj.onrender.com"]
 
 app.use(cors({
-    origin: "https://authentication-frontend-2ygj.onrender.com",
+    origin: "https://authentication-frontend-2yqj.onrender.com",
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
     allowedHeaders: ["Content-Type", "Authorization"]
